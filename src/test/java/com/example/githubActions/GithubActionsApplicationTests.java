3bcef531c2f8389ac1d36f1c359545c1e36430bb
satalla <sai_samhita_tallapragada@gap.com>
2024-03-14 16:47:33 +0530

    Initial commit

---
+++ src/test/java/com/example/githubActions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
